0865edd3521e44068f380c666eb3e294edd6ba74
Hei <lamyuenhei@gmail.com>
2014-04-26 19:09:37 +0800

    Added login api

--- negativetime/negativetime/settings.py
+++ negativetime/negativetime/settings.py
@@ -36,6 +36,10 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'docit',
 )
 
 MIDDLEWARE_CLASSES = (

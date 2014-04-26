827d034ae1d9ec9fbf52ecca61d135d40091181e
Hei <lamyuenhei@gmail.com>
2014-04-26 21:38:13 +0800

    Added list projects API

--- negativetime/docit/views.py
+++ negativetime/docit/views.py
@@ -1,12 +1,16 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import ProjectSerializer
+from .models import Project
 
 
-class ProjectCreateView(CreateAPIView):
+class ProjectListCreateView(ListCreateAPIView):
     serializer_class = ProjectSerializer
     permission_classes = (IsAuthenticated,)
 
     def pre_save(self, obj):
         obj.user = self.request.user
+
+    def get_queryset(self):
+        return Project.objects.filter(user=self.request.user)
--- negativetime/negativetime/urls.py
+++ negativetime/negativetime/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from docit.views import ProjectCreateView
+from docit.views import ProjectListCreateView
 
 
 urlpatterns = patterns('',
@@ -17,5 +17,5 @@ urlpatterns = patterns('',
                                namespace='rest_framework')),
 
 
-    url(r'^projects/$', ProjectCreateView.as_view(), name='projects'),
+    url(r'^projects/$', ProjectListCreateView.as_view(), name='projects'),
 )

3a0bc65b81037e859a3c9f23c73b78aaf8e49c8c
Hei <lamyuenhei@gmail.com>
2014-04-27 00:43:30 +0800

    Added real login views

--- negativetime/docit/views.py
+++ negativetime/docit/views.py
@@ -1,8 +1,32 @@
-from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate, logout
+from django.views.generic import View
+from django.http import HttpResponse
 
-from .serializers import ProjectSerializer
+from rest_framework.generics import ListCreateAPIView, GenericAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import ProjectSerializer, UserSerializer
 from .models import Project
+from .forms import LoginForm
+
+
+class LoginView(View):
+    def post(self, request, format=None):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.username, password=form.password)
+            if user.is_active:
+                serializer = UserSerializer(user)
+                return Response(serializer.data)
+        return HttpResponse(status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(View):
+    def get(self, request, format=None):
+        logout(request)
+        return HttpResponse(status=status.HTTP_200_OK)
 
 
 class ProjectListCreateView(ListCreateAPIView):
